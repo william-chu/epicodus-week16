f9de5ecd8af63624825e7f4f6192ac339de68ba4
William Chu <williamchu51@gmail.com>
2018-06-29 08:48:08 -0700

    create header component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import Header from './Header'
 import Error404 from './Error404';
 
 function App(){
   return (
     <div>
+      <Header />
       <Switch>
         <Route component={Error404} />
       </Switch>
