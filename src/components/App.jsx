4c313ce3cb25a7317976af0b504981cc35e65974
William Chu <williamchu51@gmail.com>
2018-06-29 09:11:12 -0700

    add header content

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,23 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import Header from './Header'
+import Header from './Header';
 import Error404 from './Error404';
 
 function App(){
   return (
     <div>
       <style jsx global>{`
+        body, html {
+          margin: 0;
+        }
+
         .app-wrapper {
           font-family: sans-serif;
         }
       `}</style>
       <div className="app-wrapper">
-        <Header />
         <Switch>
+          <Route exact path='/' component={Header} />
           <Route component={Error404} />
         </Switch>
       </div>
