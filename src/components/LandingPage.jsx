89adaafa314d1a9edcc96ecb4e643b40599e3c33
William Chu <williamchu51@gmail.com>
2018-07-06 15:58:05 -0700

    add text resize media query to LandingPage component

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -36,7 +36,22 @@ function LandingPage() {
           font-family: serif;
         }
 
-        @media (min-width: 1000px) {
+        @media (min-width: 600px) {
+          .landing-wrapper h1 {
+            font-size: 17rem;
+          }
+
+          .landing-wrapper p {
+            font-size: 2.5rem;
+          }
+
+          .landing-wrapper .author-text {
+            font-size: 2.2rem;
+            padding-left: 250px;
+          }
+        }
+
+        @media (min-width: 1200px) {
           .landing-wrapper h1 {
             font-size: 20rem;
           }
